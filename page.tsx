531f8b83a01dfcf3272d2b79c07ad36fd27d4d09
Sabahat <161129791+Sabahatsabahat@users.noreply.github.com>
2024-09-22 15:20:16 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,22 @@
+
+export default function HomePage() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-900">
+      {/* My first milestone */}
+      <h1 className="text-4xl font-bold text-center text-yellow-500 scroll-m-20 tracking-tight lg:text-5xl">
+        Hello World
+      </h1>
+      <br />
+      <br />
+      <p className="mt-6 border-l-2 pl-6 italic text-white">
+        Hi, I am a student at GIAIC, and my roll number is 104073. Sir Ameen
+        Alam assigned me this Milestone 1 task, and I have given my best effort.
+        I hope Sir likes it.
+        <br />
+        <b>Regards</b>
+        <br />
+        <span className="text-yellow-500">Sabahat</span>
+      </p>
+    </div>
+  );
+}
